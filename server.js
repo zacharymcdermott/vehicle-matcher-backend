92397ec4c3bae741cf4d1693f1babc4f2e4082c1
zacharymcdermott <zacharymcdermott18@gmail.com>
2025-11-23 14:52:20 -0600

    Return entire inventory JSON instead of first item

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ async function testLoadInventory() {
   const raw = fs.readFileSync("./inventory.json", "utf-8");
   const json = JSON.parse(raw);
 
-  return json[0];
+  return json;
 }
 
 // Basic scoring logic
